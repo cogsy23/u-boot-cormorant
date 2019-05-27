b171f217f85ef701b3a63bfc0c9e244eeafa7ac8
Ben Coughlan <ben.coughlan@cgsy.com>
2019-05-27 04:33:02 +0000

    Adding a bunch of stuff for the M2S from Emcraft

--- arch/arm/mach-m2s/cpu.c
+++ arch/arm/mach-m2s/cpu.c
@@ -4,6 +4,10 @@
  * Author(s): Ben Coughlan, <ben.coughlan@cgsy.com.au>.
  */
 #include <common.h>
+#include <envm.h>
+#include <clock.h>
+
+DECLARE_GLOBAL_DATA_PTR;
 
 #ifdef CONFIG_DISPLAY_CPUINFO
 int print_cpuinfo(void)
@@ -12,3 +16,33 @@ int print_cpuinfo(void)
 	return 0;
 }
 #endif
+
+/*
+ * CPU specific initilization
+ */
+int arch_cpu_init(void)
+{
+	/*
+	 * Initialize the eNVM driver
+	 */
+	envm_init();
+
+	/*
+	 * Initialize the timers.
+	 */
+	timer_init();
+
+	/*
+	 * Initialize the clock frequencies.
+	 */
+	clock_init();
+
+	/*
+	 * Address of the kernel boot parameters.
+	 * Use start of the external RAM for that;
+	 * kernel resides at offset 0x8000 in the external RAM.
+	 */
+	gd->bd->bi_boot_params = CONFIG_SYS_RAM_BASE;
+
+        return 0;
+}
---
+++ arch/arm/mach-m2s/soc.c
@@ -0,0 +1,89 @@
+/*
+ * (C) Copyright 2012
+ *
+ * Alexander Potashev, Emcraft Systems, aspotashev@emcraft.com
+ * Vladimir Khusainov, Emcraft Systems, vlad@emcraft.com
+ * Vladimir Skvortsov, Emcraft Systems, vskvortsov@emcraft.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston,
+ * MA 02111-1307 USA
+ */
+
+#include <common.h>
+#include <m2s.h>
+#include <asm/armv7_mpu.h>
+
+#ifdef CONFIG_M2S_CACHE_ON
+/*
+ * Enable the on-cache chip of SF2
+ */
+static void soc_cache_enable(void)
+{
+	/*
+	 * Enable cache
+	 */
+	M2S_SYSREG->cc_cr = 1 << 0;
+}
+#endif
+
+/*
+ * SoC configuration code that cannot be put into drivers
+ */
+#if defined(CONFIG_ARCH_EARLY_INIT_R)
+int arch_early_init_r(void)
+{
+
+#ifdef CONFIG_M2S_CACHE_ON
+
+	/*
+	 * Enable the on-chip cache
+	 */
+	soc_cache_enable();
+#endif
+
+#ifdef CONFIG_SYS_NS16550
+
+	/*
+	 * Reset UART0/1 and take them out of reset
+	 */
+#ifdef CONFIG_SYS_NS16550_COM1
+	M2S_SYSREG->soft_reset_cr |= (1 << 7);
+	M2S_SYSREG->soft_reset_cr &= ~(1 << 7);
+#endif
+#ifdef CONFIG_SYS_NS16550_COM2
+	M2S_SYSREG->soft_reset_cr |= (1 << 8);
+	M2S_SYSREG->soft_reset_cr &= ~(1 << 8);
+#endif
+#endif
+	/*
+	 * Configure the memory protection unit (MPU) to allow full access to
+	 * the whole 4GB address space.
+	 *
+	 * This is required, because in the default configuration code
+	 * execution is not permitted at the addresses above 0xA0000000
+	 * (including external DDR memory.)
+	 */
+
+	struct mpu_region_config cfg = {
+		.start_addr = 0x00000000,
+		.region_no = REGION_0,
+		.xn = XN_EN,
+		.ap = PRIV_RW_USR_RW,
+		.mr_attr = STRONG_ORDER,
+		.reg_size = REGION_4GB
+	};
+	mpu_config(&cfg);
+}
+#endif /* CONFIG_ARCH_EARLY_INIT_R */
